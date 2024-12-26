f6da143fc0f103de094b4065a263cc73f5acb473
Çağdaş Kotan <20231129027@ogr.akdeniz.edu.tr>
2024-12-26 19:22:17 +0300

    Add files from ZIP backup

---
+++ Internet-1-BACKUP/Internet-1/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Internet_1.Models; // AppUser'ın bulunduğu namespace'i ekleyin
+
+namespace Internet_1.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync(); // Identity kullanarak çıkış yap
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Internet-1-BACKUP/Internet-1/Models/SeedData.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Internet_1.Models
+{
+    public static class SeedData
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+
+
+
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category() { Id = 1, Name = "Categori 1", IsActive = true },
+                new Category() { Id = 2, Name = "Categori 1", IsActive = true },
+                new Category() { Id = 3, Name = "Categori 2", IsActive = true }
+);
+
+
+            modelBuilder.Entity<Product>().HasData(
+            new Product() { Id = 1, Name = "ad", Price = 10, Description = "açıklama", IsActive = true, CategoryId = 1, PhotoUrl = "", City = "Sample City" },
+            new Product() { Id = 2, Name = "ad", Price = 15, Description = "açıklama", IsActive = true, CategoryId = 1, PhotoUrl = "", City = "Sample City" },
+            new Product() { Id = 3, Name = "ad", Price = 20, Description = "açıklama", IsActive = false, CategoryId = 2, PhotoUrl = "", City = "Sample City" },
+            new Product() { Id = 4, Name = "ad", Price = 30, Description = "açıklama", IsActive = false, CategoryId = 2, PhotoUrl = "", City = "Sample City" },
+            new Product() { Id = 5, Name = "ad", Price = 25, Description = "açıklama", IsActive = false, CategoryId = 3, PhotoUrl = "", City = "Sample City" }
+            );
+
+
+
+        }
+
+    }
+}
